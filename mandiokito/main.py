eeb0c7b7fae95901ddf07cb3b7aa502d79412633
Alefe de Lima Moreira <delimaalefe@gmail.com>
2019-11-18 22:41:10 -0300

    Solves issue 1

---
+++ mandiokito/main.py
@@ -0,0 +1,14 @@
+from kivy.app import App
+from kivy.uix.screenmanager import ScreenManager, Screen
+#import telas
+
+class Gerenciador(ScreenManager):
+    def __init__(self, **kw):
+        super().__init__(**kw)
+
+class Mandiokito(App):
+    def build (self):
+        return Gerenciador()
+
+if __name__ == '__main__':
+    Mandiokito().run()
---
+++ mandiokito/telas/__init__.py
@@ -0,0 +1,5 @@
+from kivy.uix.screenmanager import Screen
+
+class TelaInicial(Screen):
+    def __init__(self, **kw):
+        super().__init__(**kw)
